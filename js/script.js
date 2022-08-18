d5305855554db0ca6d0144f41fba4ff4a8bd8546
willzfrank <willzudoh4@gmail.com>
2022-08-18 20:20:42 +0100

    added header X about mobile view

--- js/script.js
+++ js/script.js
@@ -1,70 +1,59 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
+menu.onclick = () => {
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
-}
+};
 
-document.querySelector('#login-btn').onclick = () =>{
+document.querySelector('#login-btn').onclick = () => {
   document.querySelector('.login-form-container').classList.toggle('active');
-}
+};
 
-document.querySelector('#close-login-form').onclick = () =>{
+document.querySelector('#close-login-form').onclick = () => {
   document.querySelector('.login-form-container').classList.remove('active');
-}
+};
 
-window.onscroll = () =>{
-
+window.onscroll = () => {
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
 
-  if(window.scrollY > 0){
+  if (window.scrollY > 0) {
     document.querySelector('.header').classList.add('active');
-  }else{
+  } else {
     document.querySelector('.header').classList.remove('active');
-  };
-
+  }
 };
 
-document.querySelector('.home').onmousemove = (e) =>{
-
-  document.querySelectorAll('.home-parallax').forEach(elm =>{
-
+document.querySelector('.home').onmousemove = (e) => {
+  document.querySelectorAll('.home-parallax').forEach((elm) => {
     let speed = elm.getAttribute('data-speed');
 
     let x = (window.innerWidth - e.pageX * speed) / 90;
     let y = (window.innerHeight - e.pageY * speed) / 90;
 
     elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
-
   });
-
 };
 
-
-document.querySelector('.home').onmouseleave = (e) =>{
-
-  document.querySelectorAll('.home-parallax').forEach(elm =>{
-
+document.querySelector('.home').onmouseleave = (e) => {
+  document.querySelectorAll('.home-parallax').forEach((elm) => {
     elm.style.transform = `translateX(0px) translateY(0px)`;
-
   });
-
 };
 
-var swiper = new Swiper(".vehicles-slider", {
+var swiper = new Swiper('.vehicles-slider', {
   grabCursor: true,
-  centeredSlides: true,  
+  centeredSlides: true,
   spaceBetween: 20,
-  loop:true,
+  loop: true,
   autoplay: {
     delay: 9500,
     disableOnInteraction: false,
   },
   pagination: {
-    el: ".swiper-pagination",
-    clickable:true,
+    el: '.swiper-pagination',
+    clickable: true,
   },
   breakpoints: {
     0: {
@@ -79,18 +68,18 @@ var swiper = new Swiper(".vehicles-slider", {
   },
 });
 
-var swiper = new Swiper(".featured-slider", {
+var swiper = new Swiper('.featured-slider', {
   grabCursor: true,
-  centeredSlides: true,  
+  centeredSlides: true,
   spaceBetween: 20,
-  loop:true,
+  loop: true,
   autoplay: {
     delay: 9500,
     disableOnInteraction: false,
   },
   pagination: {
-    el: ".swiper-pagination",
-    clickable:true,
+    el: '.swiper-pagination',
+    clickable: true,
   },
   breakpoints: {
     0: {
@@ -105,18 +94,18 @@ var swiper = new Swiper(".featured-slider", {
   },
 });
 
-var swiper = new Swiper(".review-slider", {
+var swiper = new Swiper('.review-slider', {
   grabCursor: true,
-  centeredSlides: true,  
+  centeredSlides: true,
   spaceBetween: 20,
-  loop:true,
+  loop: true,
   autoplay: {
     delay: 9500,
     disableOnInteraction: false,
   },
   pagination: {
-    el: ".swiper-pagination",
-    clickable:true,
+    el: '.swiper-pagination',
+    clickable: true,
   },
   breakpoints: {
     0: {
@@ -130,3 +119,45 @@ var swiper = new Swiper(".review-slider", {
     },
   },
 });
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+    navMenu.style.right = '0%';
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu');
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu');
+};
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById('header');
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add('scroll-header')
+    : header.classList.remove('scroll-header');
+};
+window.addEventListener('scroll', scrollHeader);
